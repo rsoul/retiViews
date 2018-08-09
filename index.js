58e480a5e2ff4934e3885d371e04edcbd25b1eb1
Raoul <raouldonaldn@yahoo.it>
2018-08-09 02:39:34 +0200

    Live events are now correctly displayed on client

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ function Car(id,fuel_level){
 	this.inUse=false;
 	this.color='#'+(Math.floor(Math.random()*16777215).toString(16));
 }
+
+function Trip(car,route,id){
+	this.car=car;
+	this.route=route;
+	this.id=id;
+
+}
 const availableArea=[12.38,41.82,12.60,41.96];
 function getRandomLegalPosition(){
 	return turf.randomPosition(availableArea)
@@ -27,11 +34,14 @@ function planTrip(car,destination){
 function getRoute(startingPoint,destination){
 	return ('https://api.mapbox.com/directions/v5/mapbox/driving/'+startingPoint[0]+','+startingPoint[1]+';'+destination[0]+','+destination[1]+'?geometries=geojson&access_token=pk.eyJ1IjoicnNvdWxsIiwiYSI6ImNqa2l3c2F0dTFhMTkza28zNXhnMjZ4b3UifQ.EuutLwZpj-2ZD5LVZiDlvw')
 }
-var cars=[]
+var cars=[];
+var activeTrips=[];
 
-for (var i = 1; i < 25; i++) {
+for (var i = 1; i < 20; i++) {
 	new_car=new Car(i,100)
-	cars.push(new_car)	
+	cars.push(new_car)
+	new_trip=new Trip(new_car,getRoute(new_car.pos,getRandomLegalPosition()),i);
+	activeTrips.push(new_trip);	
 }
 
 var express = require('express');
@@ -62,15 +72,15 @@ app.get('/',function(req,res){
 	res.sendFile('public/index.html', { root: __dirname })
 });
 
-io.on('connection',function(socket){
+io.sockets.on('connection',function(socket){
+	console.log('Sending cars to',socket.id)
 	for (var i = cars.length - 1; i >= 0; i--) {
 		io.emit('car-info',JSON.stringify(cars[i]))
-		console.log("sending car ",cars[i].id)
 	}
 	
-	for(var i=1;i<5;i++){
-	io.emit('route',getRoute(cars[i].pos,getRandomLegalPosition()));
-}
+	for(var i=0;i<activeTrips.length;i++){
+		io.emit('route',activeTrips[i].route,activeTrips[i].car.color);
+	}
 	})
 
 
